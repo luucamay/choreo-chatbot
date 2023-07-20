b16b60b4a282af0673b9e656df8f5b1c3e09b4f5
Lupe <lupemaydana@gmail.com>
2023-07-20 12:04:41 -0400

    feat: connect the user inputto get reuest from backend

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Choreo Chatbot
+
 A chat with the best choreographers of the industry
+
+## TODO
+
+- Review components for the clients
--- app/page.js
+++ app/page.js
@@ -1,8 +1,33 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
 import Head from 'next/head';
+import { useState } from "react";
 
 export default function Home() {
+  const [userInput, setUserInput] = useState("");
+  const [previusChats, setPreviusChats] = useState("");
+
+  const getMessages = async () => {
+    try {
+      const response = await fetch("/api/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: userInput }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className={styles.app}>
       <Head>
@@ -23,8 +48,8 @@ export default function Home() {
         </ul>
         <div className={styles.bottomsection}>
           <div className={styles.inputcontainer}>
-            <input />
-            <div id='submit' className={styles.submit}> ➣ </div>
+            <input value={userInput} onChange={(e) => { setUserInput(e.target.value) }} />
+            <div id='submit' className={styles.submit} onClick={getMessages}> ➣ </div>
           </div>
           <p className={styles.info}>
             Prevailed sincerity behaviour to so do principle mr. As departure at no propriety zealously my. On dear rent if girl view. First on smart there he sense. Earnestly enjoyment her you resources. Brother chamber ten old against. Mr be cottage so related minuter is. Delicate say and blessing ladyship exertion few margaret. Delight herself welcome against smiling its for. Suspected discovery by he affection household of principle perfectly he.
